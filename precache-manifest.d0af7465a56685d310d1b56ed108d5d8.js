28e9fbc8f9a5a3bef21d08addf8d03fa2f70da41
nishant <nishant@pop-os.localdomain>
2020-08-22 18:03:45 +0530

    Updates

---
+++ precache-manifest.d0af7465a56685d310d1b56ed108d5d8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ff0d0c2aa07f4283546fe98dff6ad72",
+    "url": "/index.html"
+  },
+  {
+    "revision": "382a3698f6b8683330f7",
+    "url": "/static/css/main.a44dda9f.chunk.css"
+  },
+  {
+    "revision": "c01088dca5b469393e86",
+    "url": "/static/js/2.ffd5355c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.ffd5355c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "382a3698f6b8683330f7",
+    "url": "/static/js/main.a4bb7734.chunk.js"
+  },
+  {
+    "revision": "98e5e8f30358bc0a9a60",
+    "url": "/static/js/runtime-main.35b931a4.js"
+  }
+]);
